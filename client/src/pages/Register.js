2a8a2e11142f958069719212ead10b2375ca48b2
Kevin Andel Josy <kevinandeljosy11@gmail.com>
2024-10-18 17:33:43 +0530

    Created login and register page

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,14 +1,14 @@
 import React from "react";
 
-function Login() {
+function Register() {
   return (
-    <form className="login">
+    <form className="register">
       <h1>Register</h1>
       <input type="text" placeholder="username" />
       <input type="password" placeholder="password" />
-      <button>Login</button>
+      <button>Register</button>
     </form>
   );
 }
 
-export default Login;
+export default Register;
